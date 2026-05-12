95f07a05b85ed449d565a71b4b2f44e9f0ddb4cf
S.Charishma <166210153+charishmasakhamuri99@users.noreply.github.com>
2026-05-12 11:30:36 +0530

    pe.v

--- pe.v
+++ pe.v
@@ -1,36 +1,39 @@
 module pe #(
-    parameter PIXEL = 8,
-    parameter WEIGHT = 8,
-    parameter PRODUCT = 24
+  parameter WEIGHT_W  = 8,
+  parameter PIXEL_W   = 8,
+  parameter PRODUCT_W = 24
 )(
-    input  logic clk, rst, load_weight,
-    input  logic [WEIGHT-1:0]  weight_in,
-    input  logic [PIXEL-1:0]   pixel_in,
-    input  logic               pixel_valid,
-    output logic signed [PRODUCT-1:0] product_out,
-    output logic               out_valid
+  input  wire                        clk,
+  input  wire                        rst,
+  input  wire                        load_weight,
+  input  wire [WEIGHT_W-1:0]         weight_in,
+  input  wire [PIXEL_W-1:0]          pixel_in,
+  input  wire                        pixel_valid,
+  output reg  signed [PRODUCT_W-1:0] product_out,
+  output reg                         out_valid
 );
 
-    logic signed [WEIGHT-1:0] weight_reg;
+  reg signed [WEIGHT_W-1:0] weight_reg;
 
-    always_ff @(posedge clk or negedge rst) begin
-        if(!rst)
-            weight_reg <= {WEIGHT{1'b0}};
-        else if(load_weight)
-            weight_reg <= weight_in;
-    end
+  always @(posedge clk or negedge rst) begin
+    if (!rst)
+      weight_reg <= {WEIGHT_W{1'b0}};
+    else if (load_weight)
+      weight_reg <= weight_in;
+  end
 
-    always_ff @(posedge clk or negedge rst) begin
-        if(!rst) begin
-            product_out <= {PRODUCT{1'b0}};
-            out_valid   <= 1'b0;
-        end
-        else begin
-            out_valid <= pixel_valid;
-            if(pixel_valid)
-                product_out <= {{8{weight_reg[WEIGHT-1]}}, weight_reg} * {{16{1'b0}}, pixel_in};
-            else
-                product_out <= {PRODUCT{1'b0}};
-        end
+  always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+      product_out <= {PRODUCT_W{1'b0}};
+      out_valid   <= 1'b0;
+    end else begin
+      out_valid <= pixel_valid;
+      if (pixel_valid)
+        product_out <= {{(PRODUCT_W-WEIGHT_W){weight_reg[WEIGHT_W-1]}}, weight_reg}
+                     * {{(PRODUCT_W-PIXEL_W){1'b0}}, pixel_in};
+      else
+        product_out <= {PRODUCT_W{1'b0}};
     end
+  end
+
 endmodule
