9fcd6153d8c1cd99e0b1195f198e8a021738de13
S.Charishma <166210153+charishmasakhamuri99@users.noreply.github.com>
2026-05-12 14:42:22 +0530

    Update line_buffer.v

--- line_buffer.v
+++ line_buffer.v
@@ -1,30 +1,28 @@
 module line_buffer #(
-  parameter PIXEL_W     = 8,
-  parameter IMAGE_WIDTH = 258
+    parameter PIXEL = 8,
+    parameter IMAGE_WIDTH = 258
 )(
-  input  wire                              clk,
-  input  wire                              wr_en,
-  input  wire [$clog2(IMAGE_WIDTH)-1:0]   wr_addr,
-  input  wire [PIXEL_W-1:0]               wr_data,
-  input  wire [$clog2(IMAGE_WIDTH)-1:0]   rd_addr,
-  output reg  [PIXEL_W-1:0]               rd_data
+    input  logic clk,
+    input  logic wr_en,
+    input  logic [$clog2(IMAGE_WIDTH)-1:0] wr_addr,
+    input  logic [PIXEL-1:0]               wr_data,
+    input  logic [$clog2(IMAGE_WIDTH)-1:0] rd_addr,
+    output logic [PIXEL-1:0]               rd_data
 );
 
-  reg [PIXEL_W-1:0] mem [IMAGE_WIDTH-1:0];
+    logic [PIXEL-1:0] mem[IMAGE_WIDTH-1:0];
 
-  integer i;
-  initial begin
-    for (i = 0; i < IMAGE_WIDTH; i = i + 1)
-      mem[i] = {PIXEL_W{1'b0}};
-  end
+    initial begin
+        for(int i=0; i<IMAGE_WIDTH; i++)
+            mem[i] = {PIXEL{1'b0}};
+    end
 
-  always @(posedge clk) begin
-    if (wr_en)
-      mem[wr_addr] <= wr_data;
-  end
+    always_ff @(posedge clk) begin
+        if(wr_en)
+            mem[wr_addr] <= wr_data;
+    end
 
-  always @(posedge clk) begin
-    rd_data <= mem[rd_addr];
-  end
-
+    always_ff @(posedge clk) begin
+        rd_data <= mem[rd_addr];
+    end
 endmodule
